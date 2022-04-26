2a3e4ac848c219f5264af467257b3da8fa0b212c
Armand Fardeau <fardeauarmand@gmail.com>
2022-04-26 18:59:47 +0200

    Force killing connection if needed

--- app/jobs/db_reset_job.rb
+++ app/jobs/db_reset_job.rb
@@ -5,19 +5,29 @@ class DBResetJob < ApplicationJob
 
   def perform
     raise "Cannot perform this action, set PERFORM_RESET=1 to execute" if ENV["PERFORM_RESET"].to_i != 1
-
-    dump_file = Dir.glob(Rails.root.join("db/dumps/*.sql")).max
     raise "A dumpfile must be provided" if dump_file.nil?
 
-    system(command(dump_file))
+    commands.each do |command|
+      system(command)
+    end
   end
 
   private
 
-  def command(dump_file)
-    # rubocop:disable Layout/LineLength
-    "psql -c 'SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity' ; psql -c 'DROP DATABASE #{database_name}' && psql -c 'CREATE DATABASE #{database_name}' && pg_restore -v -d #{database_name} #{dump_file}"
-    # rubocop:enable Layout/LineLength
+  def commands
+    [
+      "psql -c 'REVOKE CONNECT ON DATABASE #{database_name} FROM public'",
+      "psql -c 'ALTER DATABASE #{database_name} allow_connections = off'",
+      "psql -c 'SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity'",
+      "psql -c 'DROP DATABASE #{database_name} WITH (FORCE)'",
+      "psql -c 'CREATE DATABASE #{database_name}'",
+      "pg_restore -v -d #{database_name} #{dump_file}",
+      "bundle exec rails db:migrate"
+    ]
+  end
+
+  def dump_file
+    @dump_file ||= Dir.glob(Rails.root.join("db/dumps/*.sql")).max
   end
 
   def database_name
