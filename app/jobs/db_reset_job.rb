54e0017baf5f547698fccd803156907e94265832
Armand Fardeau <armandfardeau@users.noreply.github.com>
2022-05-02 11:34:48 +0200

    Reset demo (#3)
    * Reset demo
    
    * Use only pg command
    
    * Force killing connection if needed
    
    * Fix offenses
    
    * grant connection aftr drop
    
    * Revoke connect

---
+++ app/jobs/db_reset_job.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class DBResetJob < ApplicationJob
+  queue_as :reset
+
+  def perform
+    raise "Cannot perform this action, set PERFORM_RESET=1 to execute" if ENV["PERFORM_RESET"].to_i != 1
+    raise "A dumpfile must be provided" if dump_file.nil?
+
+    commands.each do |command|
+      system(command)
+    end
+  end
+
+  private
+
+  def commands
+    [
+      "psql -c 'ALTER DATABASE #{database_name} allow_connections = off'",
+      "psql -c 'SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity'",
+      "psql -c 'DROP DATABASE #{database_name} WITH (FORCE)'",
+      "psql -c 'CREATE DATABASE #{database_name}'",
+      "pg_restore -v -d #{database_name} #{dump_file}",
+      "psql -c 'ALTER DATABASE #{database_name} allow_connections = on'",
+      "bundle exec rails db:migrate"
+    ]
+  end
+
+  def dump_file
+    @dump_file ||= Dir.glob(Rails.root.join("db/dumps/*.sql")).max
+  end
+
+  def database_name
+    Rails.configuration.database_configuration[Rails.env]["database"]
+  end
+end
