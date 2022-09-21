a7777eca7b82b35461e76e4e2ea433c976786b4d
armandfardeau <fardeauarmand@gmail.com>
2022-09-21 11:56:00 +0200

    Fix offenses

--- db/migrate/20220921094834_add_polis_site_url_to_organizations.decidim_polis.rb
+++ db/migrate/20220921094834_add_polis_site_url_to_organizations.decidim_polis.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This migration comes from decidim_polis (originally 20220920152159)
 class AddPolisSiteUrlToOrganizations < ActiveRecord::Migration[5.2]
   def change
